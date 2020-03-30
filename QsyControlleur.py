566be18d1ca81bab9f3ac5ba694c53ef46557352
F8ASB <f8asb@hotmail.fr>
2020-03-30 22:57:48 +0200

    Update QsyControlleur.py

--- QsyControlleur.py
+++ QsyControlleur.py
@@ -11,7 +11,7 @@ Programme de gestion de QSY Salon
 Ce script permet lorsque qu'il est integré dans CRON de gerer un QSY
 programmé. Avant d'effectuer le QSY ce script controle que le relais
 n'est pas en émission afin d'éviter de couper un QSO
-73 & 88 de F8ASB & F8ASB Junior
+73 & 88 de F8ASB & F8ASB Junior (Thibaut)
 
 Plus d'info: F8ASB.COM
 
